4c4e2309e7e236306a29452597d4759f9159821f
Quema <myoon0929@gmail.com>
2024-08-25 14:08:27 +0900

    fix

--- skip.js
+++ skip.js
@@ -1,12 +1,20 @@
 let adskip = () => {
     console.log("Working")
-    for (let i = 0; i < document.getElementsByTagName("video").length; i++) {
-        const video = document.getElementsByTagName('video')[i];
-        if (Number.isFinite(video.duration)) {
-            video.currentTime = video.duration;
-            console.log("Ad skipped");
-        } else {
-            console.log("No ad found or invalid video duration.");
+    const daArea = document.querySelector('.da_area');
+    if (daArea) {
+        const displayStyle = window.getComputedStyle(daArea).display;
+    
+        if (displayStyle !== 'none') {
+            console.log("Element is visible");
+            for (let i = 0; i < document.getElementsByTagName("video").length; i++) {
+                const video = document.getElementsByTagName('video')[i];
+                if (Number.isFinite(video.duration)) {
+                    video.currentTime = video.duration;
+                    console.log("Ad skipped");
+                } else {
+                    console.log("No ad found or invalid video duration.");
+                }
+            }
         }
     }
 }
